4098f5eb32bb0d5f6e376824cee91f3c60066eb4
覃壮利 <qzl@bangnijiao.cn>
2017-10-14 19:26:37 +0800

    样式微调

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -38,5 +38,8 @@ export default {
   color: #2c3e50;
   width: 100%;
   padding-top: 50px;
+  .loading{
+    top:50px;
+  }
 }
 </style>
