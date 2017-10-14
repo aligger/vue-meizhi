be9fb0c629ff46d4034653a6e8e477b2b0fe338b
覃壮利 <qzl@bangnijiao.cn>
2017-10-14 08:43:15 +0800

    注册 vue-resource 模块 注册 vue-infinite-scroll 模块

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import vueResource from 'vue-resource' // 异步加载数据模块
+import infiniteScroll from 'vue-infinite-scroll' // 引入滑动模块
 // import App from './App'
 import router from './router'
 import store from './vuex/store' // 引入状态管理
@@ -8,6 +10,9 @@ import { loadFromLocalStorage } from './common/js/store' // 获取本地缓存
 import './common/css/index.styl' // 引入公共样式
 
 Vue.config.productionTip = false;
+
+Vue.use(vueResource);
+Vue.use(infiniteScroll);
 
 /* eslint-disable no-new */
 new Vue({
@@ -21,6 +26,6 @@ if (!loadFromLocalStorage('mz', 'welcome', false)) {
 }
 else {
   router.push({
-    name: 'index'
+    name: 'welfare'
   });
 }
